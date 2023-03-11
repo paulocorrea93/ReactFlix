d04b0b47054d635ee3400953d534357798ba8013
Paulo <paulocorreapc93@gmail.com>
2023-03-11 02:23:51 -0400

    adicionando arquivos iniciais do projeto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from '../components/Main'
+import Navbar from '../components/Navbar'
+<Navbar/>
+const Home = () => {
+  return (
+    <div>
+      <Main/>
+    </div>
+  )
+}
+
+export default Home
